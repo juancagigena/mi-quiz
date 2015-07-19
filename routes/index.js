05ac060dbf727dc72b1f526a5bcf92d30c51e21a
Juanca GIGENA <jcgigena@gmail.com>
2015-07-18 23:49:19 -0300

    CSS adaptable a móviles y PCs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quizController');
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'QUIZ' });
-});
+router.get('/', function(req, res) {res.render('index', { title: 'QUIZ' })});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
