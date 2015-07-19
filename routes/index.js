b5e615b492efb72cc263df61f7d8dc9b65e6211c
Juanca GIGENA <jcgigena@gmail.com>
2015-07-19 09:08:19 -0300

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,8 @@ router.get('/', function(req, res) {res.render('index', { title: 'QUIZ' })});
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+router.get('/author/creditos', function(req, res) {
+	res.render('creditos', { title: 'QUIZ-Creditos' })});
+
+
 module.exports = router;
